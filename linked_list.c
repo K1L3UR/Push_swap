ec6f441da52b86dc08533e55df70848ac8518a87
Arnaud DURAND <arnduran@paul-f4Ar7s9.clusters.42paris.fr>
2023-07-01 05:16:09 +0200

    lst chainer en cours

--- linked_list.c
+++ linked_list.c
@@ -1,25 +1,79 @@
 #include "push_swap.h"
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct s_list
 {
-	void			*content;
-	struct s_list	*next;
-}				t_list;
+	void *content;
+	struct s_list *next;
+} t_list;
 
 t_list *ft_lstnew(void *content)
 {
-	t_list	*next;
+	t_list *p;
 
-	content = malloc(sizeof(content)); 
-	next->content;
-	return(content);
+	p = (t_list *)malloc(sizeof(t_list));
+	p->content = content;
+	p->next = NULL;
+	return p;
 }
 
-int	main()
-{	 
-	t_list	*p;
+void ft_lstadd_front(t_list **lst, t_list *new)
+{
+	new->next = *lst;
+	*lst = new;
+}
 
-	p = ft_lstnew("string");
-	printf ("%s\n", *p);
-	return (0);
+void print_list(t_list **lst)
+{
+	t_list *temp = *lst;
+
+	while (temp != NULL)
+	{
+		printf("%s\n", (char *)temp->content);
+		temp = temp->next;
+	}
+}
+
+int ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
+
+t_list *ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return NULL;
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+	}
+	return lst;
+}
+
+int main()
+{
+	t_list	*p;
+	t_list	*n;
+	int		i;
+
+	p = ft_lstnew("ligne 1");
+	n = ft_lstnew("ligne 2");
+	ft_lstadd_front(&p, n);
+	n = ft_lstnew("ligne 3");
+	ft_lstadd_front(&p, n);
+	print_list(&p);
+	i = ft_lstsize(p);
+	printf("%d\n", i);
+	return 0;
 }
