9275bb5ac7c1dc82827998c4069aa63af63095fb
Arnaud DURAND <arnduran@paul-f4Br2s2.clusters.42paris.fr>
2023-09-30 20:09:10 +0200

    changement effectuer il ne reste que de s'occuper de moins de 5nb

--- parsing.c
+++ parsing.c
@@ -51,29 +51,47 @@ int parsing(int argc, char **argv, t_list **list)
 	int		i;
 	int		num;
 	t_list	*ptr;
+	char	**av;
 
 	i = 0;
 	ptr = *list;
-	while (argv[1][i] != '\0')
+	av = ft_split(argv[1], ' ');
+	while (av[i])
 	{
-		// if (ft_isdigit(argv[1][i]) || argv[1][i] == ' ' || argv[1][i] == '-') // utiliser l'atoi special ici
-		printf("%d\n", atoi_special(&argv[1][i]));
-		printf("i :%d\n", i);
-		if (atoi_special(&argv[1][i]))
-		{
-			num = ft_atoi(&argv[1][i]); // utiliser un atoi special car je dois stocker "+1 2 -3" et enlever les doublons
-			ptr = ft_lstnew(num);
+			ptr = ft_lstnew(ft_atoi(av[i]));
 			ft_lstadd_front(list, ptr);
 			double_check(*list, num);
 			i++;
-			while (ft_isdigit(argv[1][i]))
-				i++;
-		}
-		else
-			ft_error(-2);
 	}
 	return (i);
 }
+
+// int parsing(int argc, char **argv, t_list **list)
+// {
+// 	int		i;
+// 	int		num;
+// 	t_list	*ptr;
+
+// 	i = 0;
+// 	ptr = *list;
+// 	while (argv[1][i] != '\0')
+// 	{
+// 		// if (ft_isdigit(argv[1][i]) || argv[1][i] == ' ' || argv[1][i] == '-') // utiliser l'atoi special ici
+// 		if (ft_atoi(&argv[1][i]))
+// 		{
+// 			num = ft_atoi(&argv[1][i]); // utiliser un atoi special car je dois stocker "+1 2 -3" et enlever les doublons
+// 			ptr = ft_lstnew(num);
+// 			ft_lstadd_front(list, ptr);
+// 			double_check(*list, num);
+// 			i++;
+// 			while (ft_isdigit(argv[1][i]))
+// 				i++;
+// 		}
+// 		else
+// 			ft_error(-2);
+// 	}
+// 	return (i);
+// }
 
 unsigned int	find_index(t_list *list, int v)
 {
