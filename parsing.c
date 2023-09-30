be04daaf990f6ecfde118ad4844d64c7f325323a
Arnaud DURAND <arnduran@paul-f5Ar3s5.clusters.42paris.fr>
2023-09-30 19:55:40 +0200

    il faut split carrement les ' '

--- parsing.c
+++ parsing.c
@@ -3,6 +3,32 @@
 #include <stdio.h>
 #include "push_swap.h"
 
+int	atoi_special(char *str)
+{
+	int	i;
+	int	nb;
+	int	neg;
+
+	i = 0;
+	neg = 1;
+	nb = 0;
+	while (str[i] >= 8 && str[i] <= 13 || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (str[i] == '-')
+	{
+		neg = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		nb = nb * 10 + str[i] - 0;
+		i++;
+	}
+	return (nb * neg);
+}
+
 void double_check(t_list *list, int nb)
 {
 	t_list *ptr;
@@ -30,7 +56,10 @@ int parsing(int argc, char **argv, t_list **list)
 	ptr = *list;
 	while (argv[1][i] != '\0')
 	{
-		if (ft_isdigit(argv[1][i]) || argv[1][i] == ' ' || argv[1][i] == '-') // utiliser l'atoi special ici
+		// if (ft_isdigit(argv[1][i]) || argv[1][i] == ' ' || argv[1][i] == '-') // utiliser l'atoi special ici
+		printf("%d\n", atoi_special(&argv[1][i]));
+		printf("i :%d\n", i);
+		if (atoi_special(&argv[1][i]))
 		{
 			num = ft_atoi(&argv[1][i]); // utiliser un atoi special car je dois stocker "+1 2 -3" et enlever les doublons
 			ptr = ft_lstnew(num);
